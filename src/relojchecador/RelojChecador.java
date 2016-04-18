d0a7c9f496c9c712047a68348d556bae1e0f2a85
David Reyes Pucheta <davidpucheta@gmail.com>
2016-04-18 12:26:56 -0500

    Added WebCam Support

--- src/relojchecador/RelojChecador.java
+++ src/relojchecador/RelojChecador.java
@@ -10,20 +10,39 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
  * @author davidreyespucheta
  */
 public class RelojChecador extends Application {
+
     @Override
     public void start(Stage stage) throws Exception {
+
+        Stage secondStage = new Stage();
+
+        //Venatana Principal
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
+        stage.setWidth(400.00);
+        stage.setHeight(400.00);
         stage.show();
+
+        //Ventana WebCam
+        Parent secondRoot = FXMLLoader.load(getClass().getResource("/WebCam/FXML.fxml"));
+        Scene secondScene = new Scene(secondRoot);
+        secondStage.setScene(secondScene);
+        secondStage.initStyle(StageStyle.UNDECORATED);
+        secondStage.initOwner(stage);
+        secondStage.setWidth(400.00);
+        secondStage.setHeight(400.00);
+        secondStage.setX(stage.getX() + 410.00);
+        secondStage.setY(stage.getY());
+        secondStage.show();
+
     }
 
     /**
@@ -32,8 +51,5 @@ public class RelojChecador extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
-    
-    
-    
+
 }
